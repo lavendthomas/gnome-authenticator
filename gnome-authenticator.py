fde5c79daaf208c3d2abb1ab9abcd28966a16cd0
Thomas Lavend'Homme <thomas.lavend'homme@student.umons.ac.be>
2017-12-04 18:22:25 +0100

    First Attempt at a New button

--- gnome-authenticator.py
+++ gnome-authenticator.py
@@ -79,12 +79,71 @@ class MainWindow(Gtk.Window) :
 
 
     def new_service_window(self, widget) :
-        print('New !')
+        self.new_service = Gtk.Dialog()
+        self.new_service.set_transient_for(self)
+        self.new_service.set_modal(True)
+
+        # HeaderBar
+
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_show_close_button(False)
+        header_bar.props.title = 'New'
+        self.new_service.set_titlebar(header_bar)
+
+        # Add buttons and remove native x button
+
+        cancel_button = Gtk.Button(label='Cancel')
+        cancel_button.connect('clicked', self.cancel_new_service_window)
+        header_bar.pack_start(cancel_button)
+
+        ok_button = Gtk.Button(label='Ok')
+        ok_button.connect('clicked', self.ok_new_service_window)
+        header_bar.pack_end(ok_button)
+
+        # End of the HeaderBar
+
+        #TODO Use Grid instead of a listbox
+
+        content_area = self.new_service.get_content_area()
+        content_area.set_border_width(5)
+
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        content_area.add(listbox)
+
+        service_row = Gtk.ListBoxRow()
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        service_row.add(box)
+        label = Gtk.Label(label='Name of the service')
+        label.set_justify(Gtk.Justification.RIGHT)
+        self.service_entry = Gtk.Entry()
+        box.pack_start(label, True, True, 0)
+        box.pack_start(self.service_entry, True, True, 0)
+        listbox.add(service_row)
+
+        secret_key_row = Gtk.ListBoxRow()
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        secret_key_row.add(box)
+        label = Gtk.Label(label='Secret key')
+        label.set_justify(Gtk.Justification.RIGHT)
+        self.secret_key = Gtk.Entry()
+        self.secret_key.set_visibility(False)
+        box.pack_start(label, True, True, 0)
+        box.pack_start(self.secret_key, True, True, 0)
+        listbox.add(secret_key_row)
+
+        self.new_service.show_all()
+
+
+
+    def cancel_new_service_window(self, widget) :
+        self.new_service.destroy()
+
+    def ok_new_service_window(self, widget) :
         pass
 
     def remove_service_window(self, widget) :
         print('Remove !')
-        pass
 
     def refresh_codes(self, force=False) :
         global services
@@ -99,7 +158,33 @@ class MainWindow(Gtk.Window) :
             for service in services :
                 self.listboxrows[service][1].set_markup('<big><b>'+getTotpCode(keyring.get_password(SERVICENAME, service))+'</b></big>')
 
-
+# class NewServiceWindow(Gtk.Dialog) :
+#     def __int__(self, parent) :
+#         Gtk.Dialog.__init__(self, "New", parent)
+#         self.set_border_width(3)
+#         self.set_default_size(150, 100)
+#
+#         self.header_bar = Gtk.HeaderBar()
+#         self.header_bar.set_show_close_button(True) # TODO False
+#         self.set_titlebar(self.header_bar)
+#
+#         # Ok and close buttons
+#         self.close_button = Gtk.Button(label='Cancel')
+#         self.close_button.connect('clicked', self.cancel)
+#         self.header_bar.pack_end(self.close_button)
+#
+#         label = Gtk.Label("This is a dialog to display additional information")
+#
+#         box = self.get_content_area()
+#         box.add(label)
+#         self.show_all()
+#
+#     def cancel(self, widget) :
+#         # Destoy the popup window
+#         self.destoy()
+#
+#     def ok(self, widget) :
+#         pass
 
 def cleanSecret(secret) :
     returnSecret = str()
